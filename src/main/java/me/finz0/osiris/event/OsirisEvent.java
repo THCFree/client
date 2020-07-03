580bb9c8526edc0271846b7d74e51497a6abd79d
uwuhentai <55637741+uwuhentai@users.noreply.github.com>
2020-07-03 16:52:51 -0400

    eawda
    dzdzdas

--- src/main/java/me/finz0/osiris/event/OsirisEvent.java
+++
@@ -1,9 +0,0 @@
-package me.finz0.osiris.event;
-
-import me.zero.alpine.type.Cancellable;
-
-public class OsirisEvent extends Cancellable {
-
-    public OsirisEvent() {
-    }
-}
--- src/main/java/me/finz0/osiris/module/modules/chat/ToggleMsgs.java
+++
@@ -1,9 +0,0 @@
-package me.finz0.osiris.module.modules.chat;
-
-import me.finz0.osiris.module.Module;
-
-public class ToggleMsgs extends Module {
-    public ToggleMsgs() {
-        super("ToggleMsgs", Category.CHAT, "Sends a client side message when you toggle any module");
-    }
-}
