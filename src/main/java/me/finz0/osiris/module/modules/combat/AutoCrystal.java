d2e1eb2a6249d563229752c953d87b3d7d43fde2
Henry <31757267+fr0zenbyte@users.noreply.github.com>
2020-07-05 20:02:42 -0400

    whoops

--- src/main/java/me/finz0/osiris/module/modules/combat/AutoCrystal.java
+++ src/main/java/me/finz0/osiris/module/modules/combat/AutoCrystal.java
@@ -1,5 +1,6 @@
 package me.finz0.osiris.module.modules.combat;
 
+import com.mojang.realmsclient.gui.ChatFormatting;
 import me.finz0.osiris.settings.Setting;
 import me.finz0.osiris.OsirisMod;
 import me.finz0.osiris.command.Command;
