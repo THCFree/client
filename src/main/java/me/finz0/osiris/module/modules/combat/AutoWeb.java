46f9dd003361f5b1d202d24c5be20f7547816a96
Henry <31757267+fr0zenbyte@users.noreply.github.com>
2020-07-05 20:03:26 -0400

    Whoops again

--- src/main/java/me/finz0/osiris/module/modules/combat/AutoWeb.java
+++ src/main/java/me/finz0/osiris/module/modules/combat/AutoWeb.java
@@ -1,7 +1,7 @@
 package me.finz0.osiris.module.modules.combat;
 
 import java.util.List;
-
+import com.mojang.realmsclient.gui.ChatFormatting;
 import me.finz0.osiris.settings.Setting;
 import me.finz0.osiris.OsirisMod;
 import me.finz0.osiris.command.Command;
