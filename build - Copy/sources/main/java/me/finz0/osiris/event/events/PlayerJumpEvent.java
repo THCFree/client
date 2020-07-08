d3d9e3bc1b70bedbccca1d76d7c6542d50c75228
uwuhentai <55637741+uwuhentai@users.noreply.github.com>
2020-07-08 04:19:59 -0400

    dfdgs
    sdfgsdfgsdfsd

--- build - Copy/sources/main/java/me/finz0/osiris/event/events/PlayerJumpEvent.java
+++
@@ -1,9 +0,0 @@
-package me.finz0.osiris.event.events;
-
-import me.finz0.osiris.event.OsirisEvent;
-
-public class PlayerJumpEvent extends OsirisEvent {
-    public PlayerJumpEvent(){
-        super();
-    }
-}
